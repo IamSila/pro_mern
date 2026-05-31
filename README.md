61ef013559909ef778b5ca2671c023ae7c8502ee
IamSila <silamulingi@gmail.com>
2026-05-31 13:53:39 +0300

    installing plugins

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ run this to transform the jsx during build.
 
 `npx babel src --presets @babel/react --out-dir public`
 
+presets to be used can be saved in a configuration file called .babelrc [Should be placed where we have the jsx code to be transformed i.e in src folder]
+
+`polyfills` - function implementations to supplement the missing implementation in older browsers are.
+
+To include polyfills add this in html ---> use the latest versions in other project.
+`<script src="https://unpkg.com/@babel/polyfill@7/dist/polyfill.min.js"></script>`
--- public/App.js
+++ public/App.js
@@ -1,7 +1,11 @@
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const helloContinents = Array.from(continents, c => `Hello ${c}!`);
-const message = helloContinents.join(' ');
-const element = /*#__PURE__*/React.createElement("div", {
+"use strict";
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var helloContinents = Array.from(continents, function (c) {
+  return "Hello ".concat(c, "!");
+});
+var message = helloContinents.join(' ');
+var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
 }, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('contents'));
